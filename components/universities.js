9defbfdd24e481e78a860f1854775062cddb9830
SolanoFurlan <lilithfurlan@gmail.com>
2022-04-27 22:34:29 +0100

    desktop UI

---
+++ components/universities.js
@@ -0,0 +1,15 @@
+export default function Universities() {
+  return (
+    <div className="flex flex-col justify-center bg-[#FAFAFA] py-7  pb-9 pt-3  ">
+      <h1 className="text-[#373737] text-[23px] h-[100px] font-extralight pl-[600px] ">
+        Featured universities
+      </h1>
+      <div className="flex justify-evenly">
+        <img className="h-[63px]" src="images/logo1.png"></img>
+        <img className="h-[67px]" src="images/logo2.png"></img>
+        <img className="h-[67px]" src="images/logo3.png"></img>
+        <img className="h-[78px]" src="images/logo4.png"></img>
+      </div>
+    </div>
+  );
+}
